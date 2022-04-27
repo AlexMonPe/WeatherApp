6305f966d8e6e9597e96b7bb11939e97ff9232b8
Alejandro <Alex_bcn10@hotmail.es>
2022-04-27 21:12:13 +0200

    created component home and styles

--- weather-app/src/components/Home/Home.js
+++ weather-app/src/components/Home/Home.js
@@ -1,7 +1,34 @@
-const Home = () => {
-  return <div>
+import { useEffect, useState } from "react";
+import { apiConsumer } from "../../services/apiConsumer";
+
+export const Home = () => {
+  const urlTowns = "https://www.el-tiempo.net/api/json/v1/municipios";
+  const movies = "https://api-restfull-movies-nodejs.herokuapp.com/movies";
+
+  const [towns, setTowns] = useState([]);
+  useEffect(() => {
+    const loadTowns = async () => {
+      try {
+        const towns = await apiConsumer.getTowns();
+        console.log(towns, ' towns in useEffect')
+        setTowns(towns);
+      } catch (error) {
+        console.log(error, "Error in loadTowns");
+      }
+    };
+    loadTowns();
+  }, []);
+  return (
+    <div>
       <div className="home-container">
-          <p>Hola programadores</p>
+        <div className="city">Leganes</div>
+        <div className="temperature">12ºC</div>
+        <div>Rain</div>
+        <div>Max. 17º Min. 10º</div>
+        <div>amanecer: 6am Anochecher: 21pm</div>
+
+        <div>Previsiones proximos dias</div>
       </div>
-  </div>;
+    </div>
+  );
 };
